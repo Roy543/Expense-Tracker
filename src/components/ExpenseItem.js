d8c9d34593554830b8250643cebed60513056302
Roy543 <sahilsas88@gmail.com>
2023-09-08 13:46:35 +0200

    First component creation completed.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    return (
+        <div className='expense-item'>
+            <div>June 16th 2023</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$249.67</div>
+            </div>
+        </div>
+    );
+}
+export default ExpenseItem; 
